bedb2e23bb94a4bd3e32056d9ccb2e8c1e348488
Abdkalam <amjabdul@gmail.com>
2022-05-30 14:00:07 +0530

    A Codes

--- src/org/sam/StringClass.java
+++ src/org/sam/StringClass.java
@@ -8,8 +8,11 @@ public class StringClass {
 		String a1 = "system";
 		System.out.println(System.identityHashCode(a));
 		a = a.concat(a1);
+		System.out.println(System.identityHashCode(a));
 
-		
+		StringBuffer b = new StringBuffer("apple");
+		StringBuffer b1 = new StringBuffer("c");
+		System.out.println(System.identityHashCode(b));
 
 	}
 }
